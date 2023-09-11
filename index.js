60780133da81a01f9fe82c8481762141c7e23203
ballouc <96547345+ballouc@users.noreply.github.com>
2023-09-11 17:16:52 -0400

    Update files with minor bug fixes

--- index.js
+++ index.js
@@ -3,48 +3,47 @@
 let countEl = document.getElementById("count-el")
 let binomialEl = document.getElementById("binomial-distribution")
 let probabilityEl = document.getElementById("probability")
+let shinyCharmEl = document.getElementById("shiny-charm")
+let generationsEl = document.getElementById("generations")
 
 let count = 0
-let shinyOdds = 1/8192
-let masudaOdds = 1
+// Make the shiny odds update with whatever gen is currently selected :3
+let shinyOdds = 8192
+let rolls = 1
+
+// If the page is refreshed, and SV is selected as the default, it will update the rest of the webpage to reflect that.
+setValues(generationsEl)
 
 // Function which increases the value of count each time it is called, and sets the text of the count-el element 
 // in the HTML file to that value. It is called whenever the button is clicked using the on-click tag.
 
 function setValues(e){
     //Resets all of the counters
-    changeGens()
+    updateAll()
     switch(e.value){
         case "gen3":
             //Do gen 3 specific things
-            console.log("You have selected gen3")
             oldOdds()
             break;
         case "gen4":
             //etc
-            console.log("You have selected gen4")
             oldOdds()
             break;
         case "gen5":
-            console.log("You have selected gen5")
-            oldOdds()
+            // Gen 5 was the first to introduce Shiny Charm but still had old odds, so have to do this manually here
             break;
         case "gen6":
             //Do gen 6 specific things
-            console.log("You have selected gen6")
             newOdds()
             break;
         case "gen7":
             //etc
-            console.log("You have selected gen7")
             newOdds()
             break;
         case "gen8":
-            console.log("You have selected gen8")
             newOdds()
             break;
         case "gen9":
-            console.log("You have selected gen9")
             newOdds()
             break;
         default:
@@ -52,43 +51,46 @@ function setValues(e){
     }
 }
 
+shinyCharmEl.addEventListener("change", () => {
+    if (shinyCharmEl.checked){
+        rolls = 3
+        updateAll()
+    } else {
+        rolls = 1
+        updateAll()
+    }
+})
+
+// Sets new odds and updates
 function newOdds(){
-    shinyOdds = 1/4096
-    probabilityEl.textContent = "1/4096"
+    shinyOdds = 4096
+    updateAll()
 }
 
+// Sets old odds and updates
 function oldOdds(){
-    shinyOdds = 1/8192
-    probabilityEl.textContent = "1/8192"
+    shinyOdds = 8192
+    updateAll()
 }
 
-// Should I make it so it resets or just updates the probability and odds?
-function changeGens(){
-    count = 0;
-    countEl.textContent = 0;
-    binomialEl.textContent = "0.00%"
-}
-
+// Function to increase count and update on press
 function increment(){
     count += 1
     countEl.textContent = count
     updateAll()
 }
 
+// Function to decrease count and update on press
+// cannot fall below 0
 function decrement(){
     if(count > 0){
         count -= 1
         countEl.textContent = count
     }
-
-    // If count is reduced to 0, the program will not refresh binomialEl due to having 0 in the denominator
-    // This does that manually
-    if(count == 0){
-        binomialEl.textContent = "0.00%"
-    }
     updateAll()
 }
 
+// I would like this button to 'complete' the hunt and save the data
 function done(){
 
 }
@@ -100,20 +102,20 @@ function reset(){
 
 }
 
+// Calculates the probability of finding at least one shiny in 'count' encounters using selected odds
 function findBinomialDistribution(){
-    let binomialProbability = count*(shinyOdds)*((1-shinyOdds)**(count - 1))
+    let binomialProbability = count*(rolls/shinyOdds)*((1-(rolls/shinyOdds))**(count - 1))
     binomialEl.textContent = (binomialProbability * 100).toFixed(2) + "%"
-    console.log(binomialProbability)
 }
 
-//If I only have one function in this, I can remove it later and replace all instaces of 'updateAll' with whatever is inside
 function updateAll(){
     findBinomialDistribution()
+    probabilityEl.textContent = rolls + "/" + shinyOdds
 }
 
 function changeCount(){
     let temp = prompt("Please enter a number", count);
     count = Number(temp);
     countEl.textContent = count;
-    findBinomialDistribution();
+    updateAll()
 }
